1db115e3dea71192c48b72dd5694ec9c787e4b4d
TheoDel <theo.deldel@gmail.com>
2016-06-12 19:31:11 +0200

    Refactoring with adafruit library
    +servo basic integration

--- CAD/CAD.ino
+++ CAD/CAD.ino
@@ -1,152 +1,75 @@
-#include "mpr121.h"
+/*********************************************************
+  This code uses the MPR121 Adafruit library.
+  It also works for Sparkfun MPR121. (the ones I used)
+
+  Adapted by Theodel
+**********************************************************/
+
 #include <Wire.h>
+#include <Servo.h>
+#include "Adafruit_MPR121.h"
 
-int irqpin = 2;  // Digital 2
-boolean touchStates[12]; //to keep track of the previous touch states
+// Default address is 0x5A, if tied to 3.3V its 0x5B
+// If tied to SDA its 0x5C and if SCL then 0x5D
+Adafruit_MPR121 capA = Adafruit_MPR121();
+Adafruit_MPR121 capB = Adafruit_MPR121();
+Adafruit_MPR121 capC = Adafruit_MPR121();
+int capAaddr = 0x5A;
+int capBaddr = 0x5B;
+int capCaddr = 0x5C;
 
-void setup(){
+int irqpin = 2;
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+//Servomoteurs
+Servo servoA;
+Servo servoB;
+Servo servoC;
+int servoApin = 9;
+int servoBpin = 10;
+int servoCpin = 11;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("Initialisation MPR121 Capacitive Touch sensor");
+  capA.begin(capAaddr);
+
+  servoA.attach(9);
+
   pinMode(irqpin, INPUT);
   digitalWrite(irqpin, HIGH); //enable pullup resistor
-  
-  Serial.begin(9600);
-  Wire.begin();
-
-  mpr121_setup();
+  Serial.println("Fin du setup");
 }
 
-void loop(){
-  readTouchInputs();
-  //delay(200);
-}
+void loop() {
+  if (!checkInterrupt()) {
+    // Get the currently touched pads
+    currtouched = capA.touched();
 
-
-void readTouchInputs(){
-  if(!checkInterrupt()){
-    
-    //read the touch state from the MPR121
-    Wire.requestFrom(0x5A,2); 
-    
-    byte LSB = Wire.read();
-    byte MSB = Wire.read();
-    
-    uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
-
-    
-    for (int i=0; i < 12; i++){  // Check what electrodes were pressed
-      if(touched & (1<<i)){
-      
-        if(touchStates[i] == 0){
-          //pin i was just touched
-          Serial.println(i);
-        
-        }else if(touchStates[i] == 1){
-          //pin i is still being touched
-        }  
-      
-        touchStates[i] = 1;      
-      }else{
-        if(touchStates[i] == 1){/*
-          Serial.print("pin ");
-          Serial.print(i);
-          Serial.println(" is no longer being touched");*/
-          
-          //pin i is no longer being touched
-       }
-        
-        touchStates[i] = 0;
+    for (uint8_t i = 0; i < 12; i++) {
+      // it if *is* touched and *wasnt* touched before, alert!
+      if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+        Serial.print(i); Serial.println(" touched");
+        servoA.write(map(i, 0, 11, 0, 180));
       }
-    
+      // if it *was* touched and now *isnt*, alert!
+      if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+        Serial.print(i); Serial.println(" released");
+      }
     }
-    
+
+    // reset our state
+    lasttouched = currtouched;
   }
+  delay(50);
 }
-
-
-
-
-void mpr121_setup(void){
-
-  set_register(0x5A, ELE_CFG, 0x00); 
-  
-  // Section A - Controls filtering when data is > baseline.
-  set_register(0x5A, MHD_R, 0x01);
-  set_register(0x5A, NHD_R, 0x01);
-  set_register(0x5A, NCL_R, 0x00);
-  set_register(0x5A, FDL_R, 0x00);
-
-  // Section B - Controls filtering when data is < baseline.
-  set_register(0x5A, MHD_F, 0x01);
-  set_register(0x5A, NHD_F, 0x01);
-  set_register(0x5A, NCL_F, 0xFF);
-  set_register(0x5A, FDL_F, 0x02);
-  
-  // Section C - Sets touch and release thresholds for each electrode
-  set_register(0x5A, ELE0_T, TOU_THRESH);
-  set_register(0x5A, ELE0_R, REL_THRESH);
- 
-  set_register(0x5A, ELE1_T, TOU_THRESH);
-  set_register(0x5A, ELE1_R, REL_THRESH);
-  
-  set_register(0x5A, ELE2_T, TOU_THRESH);
-  set_register(0x5A, ELE2_R, REL_THRESH);
-  
-  set_register(0x5A, ELE3_T, TOU_THRESH);
-  set_register(0x5A, ELE3_R, REL_THRESH);
-  
-  set_register(0x5A, ELE4_T, TOU_THRESH);
-  set_register(0x5A, ELE4_R, REL_THRESH);
-  
-  set_register(0x5A, ELE5_T, TOU_THRESH);
-  set_register(0x5A, ELE5_R, REL_THRESH);
-  
-  set_register(0x5A, ELE6_T, TOU_THRESH);
-  set_register(0x5A, ELE6_R, REL_THRESH);
-  
-  set_register(0x5A, ELE7_T, TOU_THRESH);
-  set_register(0x5A, ELE7_R, REL_THRESH);
-  
-  set_register(0x5A, ELE8_T, TOU_THRESH);
-  set_register(0x5A, ELE8_R, REL_THRESH);
-  
-  set_register(0x5A, ELE9_T, TOU_THRESH);
-  set_register(0x5A, ELE9_R, REL_THRESH);
-  
-  set_register(0x5A, ELE10_T, TOU_THRESH);
-  set_register(0x5A, ELE10_R, REL_THRESH);
-  
-  set_register(0x5A, ELE11_T, TOU_THRESH);
-  set_register(0x5A, ELE11_R, REL_THRESH);
-  
-  // Section D
-  // Set the Filter Configuration
-  // Set ESI2
-  set_register(0x5A, FIL_CFG, 0x04);
-  
-  // Section E
-  // Electrode Configuration
-  // Set ELE_CFG to 0x00 to return to standby mode
-  set_register(0x5A, ELE_CFG, 0x0C);  // Enables all 12 Electrodes
-  
-  
-  // Section F
-  // Enable Auto Config and auto Reconfig
-  /*set_register(0x5A, ATO_CFG0, 0x0B);
-  set_register(0x5A, ATO_CFGU, 0xC9);  // USL = (Vdd-0.7)/vdd*256 = 0xC9 @3.3V   set_register(0x5A, ATO_CFGL, 0x82);  // LSL = 0.65*USL = 0x82 @3.3V
-  set_register(0x5A, ATO_CFGT, 0xB5);*/  // Target = 0.9*USL = 0xB5 @3.3V
-  
-  set_register(0x5A, ELE_CFG, 0x0C);
-  
-}
-
 
 boolean checkInterrupt(void){
   return digitalRead(irqpin);
-}
-
-
-void set_register(int address, unsigned char r, unsigned char v){
-    Wire.beginTransmission(address);
-    Wire.write(r);
-    Wire.write(v);
-    Wire.endTransmission();
 }
